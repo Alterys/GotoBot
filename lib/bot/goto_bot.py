732f66e19d18b96bad5434e0dfa453c1a64d3c00
Altery <krgtskrgts005@gmail.com>
2023-02-12 18:22:43 +0300

    edit goto_bot.py (updated error visual)

--- lib/bot/goto_bot.py
+++ lib/bot/goto_bot.py
@@ -1,3 +1,4 @@
+import discord
 from discord import Intents, Status, Activity, ActivityType, TextChannel
 from discord.errors import Forbidden, HTTPException
 from discord.ext.commands import Bot as DisBot
@@ -43,18 +44,20 @@ class Bot(DisBot):
 
     async def on_command_error(self, ctx, exc):
         if isinstance(exc, CommandNotFound):
-            await ctx.send("Команда не найдена.")
+            await ctx.send(embed=discord.Embed(title='Команда не найдена', color=0x36393e))
         elif isinstance(exc, MissingPermissions):
-            await ctx.send("У тебя недостаточно прав использовать эту команду!")
+            await ctx.send(embed=discord.Embed(
+                title='У тебя недостаточно прав использовать эту команду!',
+                color=0x36393e))
         elif isinstance(exc, MissingRequiredArgument):
-            await ctx.send("Добавь необходимые аргументы!")
+            await ctx.send(embed=discord.Embed(title='Добавь необходимые аргументы', color=0x36393e))
         elif isinstance(exc, BadArgument):
-            await ctx.send("Ты ввел что-то неверно, попробуй ещё раз.")
+            await ctx.send(embed=discord.Embed(title='Ты ввел что-то неверно, попробуй ещё раз', color=0x36393e))
         elif hasattr(exc, "original"):
             if isinstance(exc.original, HTTPException):
                 pass
             elif isinstance(exc.original, Forbidden):
-                await ctx.send("У меня нет на это разрешения")
+                await ctx.send(embed=discord.Embed(title='У меня нет на это разрешения', color=0x36393e))
         elif hasattr(exc, "original"):
             raise exc.original
         else:
@@ -62,7 +65,7 @@ class Bot(DisBot):
 
     async def on_ready(self):
         await self.update_db()
-        await self.change_presence(activity=Activity(name="", type=ActivityType.listening))
+        await self.change_presence(activity=Activity(name="https://discord.gg/gx2wCSPspB", type=ActivityType.listening))
         print("bot ready..")
 
 
